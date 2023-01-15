f6dc9b32aebc1dbea7d5879107a06a9c5ac58f86
smulczynskasia <smulczynska.katarzyna@gmail.com>
2023-01-15 12:19:12 +0100

    Add new files

---
+++ src/components/ResultBox/ResultBox.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
+import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
+import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
+import { useMemo } from 'react';
+import styles from './ResultBox.module.scss';
+
+const ResultBox = ({ from, to, amount }) => {
+
+  const convertedAmount = useMemo(() => {
+    if(amount < 0 ) return 'Wrong value';
+    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
+    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    return formatAmountInCurrency(amount, from);
+  }, [from, to, amount]);
+
+  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
+
+  return (
+    <div className={styles.result} data-testid="output">
+      {formattedAmount} = {convertedAmount}
+    </div>
+  );
+};
+
+ResultBox.propTypes = {
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+}
+
+export default ResultBox;
